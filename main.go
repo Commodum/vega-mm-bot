d23c0a96634194a0dc4ed0d29d5421ecd8b2d266
ed <edward@commodum.io>
2023-07-14 09:58:33 +0000

    Initial groundwork, started defining data store and started setting up flags for config.

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+
+	wallet "github.com/jeremyletang/vega-go-sdk/wallet"
+)
+
+const (
+	defaultAdminPort			= 8080
+	defaultVegaGrpcAddr 		= "tls://vega-mainnet-data-grpc.commodum.io:443" // "vega-data.nodes.guru:3007" "vega-data.bharvest.io:3007" "datanode.vega.pathrocknetwork.org:3007"
+	defaultBinanceWsAddr 		= "wss://ws-api.binance.com:443/ws-api/v3"
+	defaultWalletServiceAddr 	= "http://127.0.0.1:1789"
+)
+
+var (
+	adminPort 			int
+	vegaGrpcAddr		string
+	binanceWsAddr		string
+	walletServiceAddr 	string
+	walletToken			string
+	walletPubkey		string
+	vegaMarket			string
+	binanceMarket		string
+)
+
+func init() {
+	fmt.Println("Initializing..")
+	flag.Uintvar(&adminPort, "admin-port", defaultAdminApiPort, "The port for the Admin API")
+	flag.StringVar(&vegaGrpcAddr, "vega-grpc-addr", defaultVegaGrpcAddr, "A vega grpc server")
+	flag.StringVar(&binanceWsAddr, "binance-ws-addr", defaultBinanceWsAddr, "A Binance websocket url")
+
+	flag.StringVar(&walletURL, "wallet-url", defaultWalletURL, "a vega wallet service address")
+	flag.StringVar(&walletToken, "wallet-token", "", "a vega wallet token (for info see vega wallet token-api -h)")
+	flag.StringVar(&walletPubkey, "wallet-pubkey", "", "a vega public key")
+	flag.StringVar(&vegaMarket, "vega-market", "", "a vega market id")
+	flag.StringVar(&binanceMarket, "binance-market", "", "a binance market symbol")
+
+
+}
+
+func main() {
+
+	// Things we need to do:
+	// 	a). Connect to wallet
+	//	b). Init data store
+	//	c). Subscribe to external prices
+	//	d). Get current bot state from Vega
+	//	e). Subscribe to vega data
+	//	f). Run strategy
+
+	// a).
+	w, err := wallet.NewClient(defaultWalletServiceAddr, token)
+	if err != nil {
+		log.Fatalf("Could not connect to wallet: %v", err);
+	}
+
+	// b).
+	store := newDataStore(binanceMarket)
+	client := initDataClient()
+
+	// c).
+	go client.streamBinanceData(config, dataStore)
+
+	go client.streamVegaData(config, dataStore)
+
+}
---
+++ store.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/gorilla/websocket"
+	"github.com/shopspring/decimal"
+	apipb "code.vegaprotocol.io/vega/protos/data-node/api/v2"
+	vegapb "code.vegaprotocol.io/vega/protos/vega"
+)
+
+type VegaStore struct {
+	mu sync.RWMutex
+
+	marketId string
+	market *vegapb.Market
+	marketData *vegapb.MarketData
+	accounts map[string]*apipb.AccountBalance
+	orders map[string]*vegapb.Order
+	position *vegapb.Position
+}
+
+type BinanceStore struct {
+	mu sync.RWMutex
+
+	market string
+	bestBid decimal.Decimal
+	bestAsk decimal.Decimal
+}
+
+type DataStore struct {
+	v *VegaStore
+	b *BinanceStore
+}
+
+func newVegaStore() VegaStore {
+	return &VegaStore{
+		accounts: map[string]*apipb.AccountBalance{},
+		orders: map[string]*vegapb.Order{},
+	}
+}
+
+func newBinanceStore(mkt string) BinnaceStore {
+	return &BinanceStore{
+		market: mkt,
+	}
+}
+
+func newDataStore(binanceMkt string) DataStore {
+	return &DataStore{
+		v: newVegaStore(),
+		b: newBinanceStore(binanceMkt),
+	}
+}
+
+func (v *VegaStore) SetMarketId(marketId string) {
+	v.mu.Lock()
+	defer v.mu.Unlock()
+	v.marketId = marketId
+}
+
+func (v *VegaStore) GetMarketId() string {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+	return v.marketId
+}
+
+func (v *VegaStore) SetMarket(market *vegapb.Market) {
+	v.mu.Lock()
+	defer v.mu.Unlock()
+	v.market = market
+}
+
+func (v *VegaStore) GetMarket() *vegapb.Market {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+	return v.market
+}
+
+func (v *VegaStore) SetMarketData(marketData *vegapb.MarketData) {
+	v.mu.Lock()
+	defer v.mu.Unlock()
+	v.marketData = marketData
+}
+
+func (v *VegaStore) GetMarketData() *vegapb.MarketData {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+	return v.marketData
+}
+
+func (v *VegaStore) SetAccounts(accounts []*apipb.AccountBalance) {
+	v.mu.Lock()
+	defer v.mu.Unlock()
+
+	for _, acc := range accounts {
+		v.accounts[acc.Type.String() + acc.Asset + acc.MarketId] = acc
+	}
+}
+
+func (v *VegaStore) GetAccounts() []*apipb.AccountBalance {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+	return maps.Values(v.accounts)
+}
+
+func (v *VegaStore) SetOrders(orders []*vegapb.Order) {
+	v.mu.Lock()
+	defer v.mu.Unlock()
+
+	for _, ord := range orders {
+		if ord.Status != vegapb.Order_STATUS_ACTIVE {
+			delete(v.orders, ord.Id)
+			continue
+		}
+		
+		v.orders[ord.Id] = ord
+	}
+}
+
+func (v *VegaStore) GetAccounts() []*vegapb.Order {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+	return maps.Values(v.orders)
+}
+
+func (v *VegaStore) SetPosition(position *vegapb.Position) {
+	v.mu.Lock()
+	defer v.mu.Unlock()
+	v.position = position
+}
+
+func (v *VegaStore) GetPosition() *vegapb.Position {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+	return v.position
+}
